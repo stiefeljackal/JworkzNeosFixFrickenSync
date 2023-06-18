23a2ca9ba053b55c0ddc6fc12033626f1f6aa1c3
Jarl Gullberg <jarl.gullberg@gmail.com>
2023-06-18 21:54:17 +0200

    Implement retry logic with an optional delay.

--- JworkzNeosFixFrickenSync/JworkzNeosFixFrickenSync.cs
+++ JworkzNeosFixFrickenSync/JworkzNeosFixFrickenSync.cs
@@ -1,6 +1,8 @@
-﻿using NeosModLoader;
+﻿using System;
+using NeosModLoader;
 using HarmonyLib;
 using FrooxEngine;
+using JworkzNeosMod.Patches;
 
 namespace JworkzNeosMod
 {
@@ -14,6 +16,14 @@ namespace JworkzNeosMod
         [AutoRegisterConfigKey]
         private static readonly ModConfigurationKey<bool> KEY_ENABLE =
             new ModConfigurationKey<bool>("enabled", $"Enables the {nameof(JworkzNeosFixFrickenSync)} mod", () => true);
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<byte> KEY_RETRY_COUNT =
+            new ModConfigurationKey<byte>("retryCount", "The number of times to retry failed sync actions.", () => 3);
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<TimeSpan> KEY_RETRY_DELAY =
+            new ModConfigurationKey<TimeSpan>("retryDelay", "The delay between attempts to retry failed sync actions.", () => TimeSpan.Zero);
 
         private static ModConfiguration Config;
 
@@ -36,11 +46,17 @@ namespace JworkzNeosMod
             Config.OnThisConfigurationChanged += OnConfigurationChanged;
             Engine.Current.OnReady += OnCurrentNeosEngineReady;
 
+            RecordUploadTaskBasePatch.MaxUploadRetries = Config.GetValue(KEY_RETRY_COUNT);
+            RecordUploadTaskBasePatch.RetryDelay = Config.GetValue(KEY_RETRY_DELAY);
+
             _harmony.PatchAll();
         }
 
         private void RefreshMod()
         {
+            RecordUploadTaskBasePatch.MaxUploadRetries = Config.GetValue(KEY_RETRY_COUNT);
+            RecordUploadTaskBasePatch.RetryDelay = Config.GetValue(KEY_RETRY_DELAY);
+
             var isEnabled = Config.GetValue(KEY_ENABLE);
             ToggleHarmonyPatchState(isEnabled);
         }
--- JworkzNeosFixFrickenSync/Patches/RecordUploadTaskBasePatch.cs
+++ JworkzNeosFixFrickenSync/Patches/RecordUploadTaskBasePatch.cs
@@ -16,32 +16,58 @@ namespace JworkzNeosMod.Patches
         private static readonly FieldInfo _completionSourceField = AccessTools.Field(typeof(RecordUploadTaskBase<FrooxEngine.Record>), "_completionSource");
         private static readonly MethodInfo _failMethod = AccessTools.Method(typeof(RecordUploadTaskBase<FrooxEngine.Record>), "Fail");
 
+        internal static byte MaxUploadRetries { get; set; }
+        internal static TimeSpan RetryDelay { get; set; }
+
         [HarmonyPrefix]
         [HarmonyPatch("RunUpload")]
-        static bool RunUploadPrefix(RecordUploadTaskBase<FrooxEngine.Record> __instance, ref Task __result, CancellationToken cancellationToken)
+        static bool RunUploadPrefix(RecordUploadTaskBase<FrooxEngine.Record> __instance, out Task __result, CancellationToken cancellationToken)
         {
             var uploadTask = (Task)_runUploadInternalMethod.Invoke(__instance, new object[] { cancellationToken });
             var completionSource = (TaskCompletionSource<bool>)_completionSourceField.GetValue(__instance);
             var failMethod = _failMethod.CreateDelegate<Action<string>>(__instance);
 
-            __result = Task.Run(async () =>
-            {
-                try
+            // We don't want to let the task terminate early, so the cancellation token is not passed on
+            __result = Task.Run
+            (
+                async () =>
                 {
-                    NeosMod.Msg("Starting sync task...");
-                    await uploadTask.ConfigureAwait(false);
-                    NeosMod.Msg("Sync Upload Task is done");
-                    if (completionSource != null && !completionSource.Task.IsCompleted)
+                    var retryCount = 0;
+                    var maxRetryCount = MaxUploadRetries <= 0 ? 1 : MaxUploadRetries;
+                    var delay = RetryDelay;
+
+                    while (retryCount < maxRetryCount && !cancellationToken.IsCancellationRequested)
                     {
-                        completionSource.SetResult(__instance.IsFinished);
+                        try
+                        {
+                            await uploadTask.ConfigureAwait(false);
+                            _ = completionSource.TrySetResult(__instance.IsFinished);
+
+                            return;
+                        }
+                        catch (Exception ex)
+                        {
+                            NeosMod.Error($"Exception during record upload task (attempt {retryCount + 1} out of {maxRetryCount}): {ex}");
+                        }
+
+                        ++retryCount;
+                        await Task.Delay(delay, CancellationToken.None);
                     }
-                }
-                catch (Exception ex)
-                {
-                    NeosMod.Error($"Exception during record upload task:\n {ex?.ToString()}");
-                    failMethod($"Exception during sync.");
-                }
-            });
+
+                    // we may not have signalled completion at this point, so if we haven't, notify the system of failure
+                    if (!completionSource.Task.IsCompleted)
+                    {
+                        failMethod
+                        (
+                            cancellationToken.IsCancellationRequested
+                                ? "Record upload task cancelled"
+                                : "Exception during sync."
+                        );
+                    }
+                },
+                CancellationToken.None
+            );
+
             return false;
         }
     }
