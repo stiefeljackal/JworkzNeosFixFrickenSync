147c0eec46ec1b6de48a7601c1a7cde7fe3bc109
StiefelJackal <stiefel@jackalworkz.net>
2023-07-22 18:00:12 -0400

    fix: correct mod Link

--- JworkzNeosFixFrickenSync/JworkzNeosFixFrickenSync.cs
+++ JworkzNeosFixFrickenSync/JworkzNeosFixFrickenSync.cs
@@ -12,7 +12,7 @@ namespace JworkzNeosMod
         public override string Name => nameof(JworkzNeosFixFrickenSync);
         public override string Author => "Stiefel Jackal";
         public override string Version => "1.1.0";
-        public override string Link => "https://github.com/stiefeljackal/NeosFixFrickenSync";
+        public override string Link => "https://github.com/stiefeljackal/JworkzNeosFixFrickenSync";
 
         [AutoRegisterConfigKey]
         private static readonly ModConfigurationKey<bool> KEY_ENABLE =
